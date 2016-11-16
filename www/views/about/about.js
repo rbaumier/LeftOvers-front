1bbb1c494553a5c9456b0441526fdec4c555bea1
walrand degeorges <w.degeorges@gmail.com>
2016-11-16 15:31:36 +0100

    About v1maybe

--- www/views/about/about.js
+++ www/views/about/about.js
@@ -5,7 +5,7 @@ angular.module('App').controller('AboutCtrl', function ($scope, $http, $timeout,
 
 		{
 		 name: 	"potata",
-		 url:  	"http://66.media.tumblr.com/b7009f619314ad5eafbc887af573ef37/tumblr_inline_ogon1cVeds1rfztmg_500.gif"
+		 url:  	"http://66.media.tumblr.com/586b9bebe68367a7ae7d9a52b07449a4/tumblr_ogo8lohAhU1tki7xko1_400.jpg"
 		},
 		{
 		 name: 	"pototo",
@@ -14,10 +14,17 @@ angular.module('App').controller('AboutCtrl', function ($scope, $http, $timeout,
 		{
 		 name: 	"potete",
 		 url: 	"http://65.media.tumblr.com/c60727b107659bc54792cef4bc5d6da9/tumblr_ml1qlwUKod1qanqxuo1_400.jpg"
+		},
+		{
+		 name: 	"testa",
+		 url: 	"http://66.media.tumblr.com/b7009f619314ad5eafbc887af573ef37/tumblr_inline_ogon1cVeds1rfztmg_500.gif"
 		}
 
 	];
 
+	$http.get('http://leftovers.jlitaize.fr/api/dealers').success(function (data) {
+    	$scope.dealers = data;
+  	});
 
 
 });
